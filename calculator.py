498fd3c0d7dc62e3924c3e43c45a6417a606b5d8
Ashwin Anhal <Ash@192.168.1.109>
2024-01-25 18:16:32 +0000

    changed to float values for the final calculations

--- calculator.py
+++ calculator.py
@@ -15,31 +15,32 @@ if user_entry != "investment" and user_entry != "bond" :
 
 #Calculating the total amount of money if the user inputs investment 
 if user_entry == "investment" :
-    deposit  = int(input("Please enter the amount of money you are depositing (in £) : "))
-    rate     = int(input("Please enter the interest rate (in %) : "))
-    time     = int(input("Please enter the number of years you plan on investing for : "))
+    deposit  = float(input("Please enter the amount of money you are depositing (in £) : "))
+    rate     = float(input("Please enter the interest rate (in %) : "))
+    time     = float(input("Please enter the number of years you plan on investing for : "))
     interest = input("Please enter if you would like simple or compound interest? : ").lower() 
 
     # Calculation of the total amount of money depending on the user's interest input
     if interest == "simple" :
-        total_amount = int(deposit * (1 + (rate / 100) * time))
+        total_amount = float(deposit * (1 + (rate / 100) * time))
         print(f"Your total simple amount after {rate} years is : £ {total_amount}")
 
     elif interest == "compound" :
-        total_amount = int(deposit * math.pow((1 + (rate / 100)), time))
+        total_amount = float(deposit * math.pow((1 + (rate / 100)), time))
         print(f"Your total compounded amount after {rate} years is : £ {total_amount}")
 
     else :
-        print("You have not entered a valid input. Please try again.")
+        print("You have not entered a valid input, please try again.")
+
     
 
 #Calulating the total amount of money if the user inputs bond 
 if user_entry == "bond" :
-    present_value = int(input("Please enter the present value of your house : "))
-    rate          = int(input("Please enter the interest rate (in %) : "))
-    time          = int(input("Please enter the number of months you plan to take to repay the bond : "))
+    present_value = float(input("Please enter the present value of your house : "))
+    rate          = float(input("Please enter the interest rate (in %) : "))
+    time          = float(input("Please enter the number of months you plan to take to repay the bond : "))
     
-    i = (rate / 100) / 12  #Calculating the monthly interest rate
-    repayment = int((i * present_value) / (1 - ((1 + i) ** (-1 * time))))
+    i_rate = (rate / 100) / 12  #Calculating the monthly interest rate
+    repayment = float((i_rate * present_value) / (1 - ((1 + i_rate) ** (-1 * time))))
 
     print(f"Your total monthly repayment will be £{repayment}")
